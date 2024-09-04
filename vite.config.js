36305eedfd15d85265809411539fe8c31fd9c2c7
Stevan <mitricstevan5@gmail.com>
2024-09-04 17:33:10 +0200

    phase 1 deploy 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-management-dashboard',
+  build: {
+    outDir: 'dist',
+  },
 });
