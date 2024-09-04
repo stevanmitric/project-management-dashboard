57a8d8f998be90b3250e7f95115a5fe897f6bea2
Stevan <mitricstevan5@gmail.com>
2024-09-04 21:32:44 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-  server: {
-    port: 3000, // The port number the development server will listen on
   },
 });
